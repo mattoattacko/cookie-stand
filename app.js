d3f489f031cc888c0c788cd74bd68173204c7692
Matt McQuain <matt.mcquain@gmail.com>
2018-05-17 21:25:05 -0700

    first file commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// //var pike = {
+//     min: 3,
+//     max: 9,
+//     avg: 6.1,
+//     getRandom: function(min, max) {
+//         return Math.random() * (max-min) + min;
+//     }
+// }
+
+// pikePlace.getRandom(pikePlace.min, pikePlace.max)
+
+// //var h1El = document.createElement('h1') creates an element with js but we have not attached it to the browser yet
+// //latch onto any 
+
+// //look up MDN document.createElement
